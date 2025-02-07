0221a13478ab8660db369f2c2a189a436e615231
Johnathan Rocha <johnathanrocha@gmail.com>
2025-02-07 20:13:46 -0300

    feat: remove unused entity exports from entities.dart

--- lib/domain/entities/entities.dart
+++
@@ -1,8 +0,0 @@
-export 'category_item_entity.dart';
-export 'discount_entity.dart';
-export 'item_suite_entity.dart';
-export 'motel_data_entity.dart';
-export 'motel_entity.dart';
-export 'motel_response_entity.dart';
-export 'period_entity.dart';
-export 'suite_entity.dart';
